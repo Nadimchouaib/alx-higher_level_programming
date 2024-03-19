24bb3682267448439907b248bbc1a885bcbec90e
Nadimchouaib <ndm.bouchaib@gmail.com>
2024-03-19 04:17:14 +0000

    task 10-1

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 
 def divisible_by_2(my_list=[]):
     tmp = []
-    for index in range(len(tmp)):
+    for index in range(len(my_list)):
         if my_list[index] % 2 == 0:
             tmp.append(True)
         else:
