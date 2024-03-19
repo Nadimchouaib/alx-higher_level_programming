88163b662e89817b92b316abe60421218a45b4b7
Nadimchouaib <ndm.bouchaib@gmail.com>
2024-03-19 02:24:49 +0000

    task 1-2

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    """function that retrieves an element from a list like in C
 
-    Parameters:
-        my_list: a list
-        idx: the index of item to retrieve
-
-    Returns:
-        the item at a given index idx
-    """
     if my_list:
         if idx < 0:
             return
