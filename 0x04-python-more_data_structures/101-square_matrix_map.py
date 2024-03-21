d2ce7b1387cab5a93c603b2ff0969dd002e867de
Nadimchouaib <ndm.bouchaib@gmail.com>
2024-03-21 02:49:30 +0000

    advanced 3

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     squared_matrix = []
-    
     for row in matrix:
         squared_row = list(map(lambda i: i ** 2, row))
         squared_matrix.append(squared_row)
